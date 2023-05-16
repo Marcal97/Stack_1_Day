13b9b2da0299413a99b43cbdab4499d1a6c7f471
Mario Calise <hh2@nimbusintelligence.com>
2023-05-16 08:40:27 +0000

    Added hook for security to the employees table in production

--- models/Marts/SQL/PRD_Employees.sql
+++ models/Marts/SQL/PRD_Employees.sql
@@ -1,3 +1,12 @@
+{{ config(
+  post_hook = "ALTER TABLE {{ this }} ADD ROW ACCESS POLICY Analytics_DB.Policies.EEmployee_policy ON (Team);
+            
+               GRANT USAGE ON SCHEMA Analytics_DB.Production TO ROLE Team_Manager_1;
+               GRANT SELECT ON TABLE Analytics_DB.Production.PRD_Employees TO ROLE Team_Manager_1;
+               GRANT USAGE ON SCHEMA Analytics_DB.Production TO ROLE Team_Manager_2;
+               GRANT SELECT ON TABLE Analytics_DB.Production.PRD_Employees TO ROLE Team_Manager_2;"
+) }}
+
 WITH 
 
 Source1 AS (
